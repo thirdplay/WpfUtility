cd9c53505f6630c7d49d7f2b91616d3d7dc7aa53
thirdplay <thirdplay@outlook.com>
2019-03-14 20:13:47 +0900

    PropertyChangedExtensionsの廃止

--- source/WpfUtility/Properties/AssemblyInfo.cs
+++ source/WpfUtility/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Windows.Markup;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
